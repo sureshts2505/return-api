f3b863a85177a866f6e280cb00c34fbd27cbd043
suresh <sureshts2505@gmail,com>
2026-04-09 17:38:19 +0530

    backend updated with multer and image handling

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-require("./db")
-const express = require("express")
-const cors = require("cors")
+require("./db");
+
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const lostRoutes = require("./routes/lostRoutes");
-const foundRoutes = require("./routes/foundRoutes")
-const authRoutes = require("./routes/authRoutes")
+const foundRoutes = require("./routes/foundRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 
-app.use("/uploads/lost", express.static(path.join(__dirname, "uploads/lost")));
-app.use("/uploads/found", express.static(path.join(__dirname, "uploads/found")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
+// ✅ ROUTES
 app.use("/lost", lostRoutes);
-app.use("/found" , foundRoutes)
-app.use("/auth" , authRoutes)
+app.use("/found", foundRoutes);
+app.use("/auth", authRoutes);
 
 
-app.listen(5000, () => console.log("Server Started"))
+
+app.listen(5000, () => console.log("Server Started on port 5000"));
