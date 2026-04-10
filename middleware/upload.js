2d2586f06688532cc2b6d75d5603b11f7f15e49d
suresh <sureshts2505@gmail,com>
2026-04-10 10:28:37 +0530

    Final fix for upload config

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,9 +6,26 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "returnMe",
+    resource_type: "image",
+
+    
+    public_id: (req, file) => {
+      return Date.now() + "-" + file.originalname;
+    },
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+
+  
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files allowed"), false);
+    }
+  },
+});
 
 module.exports = upload;
