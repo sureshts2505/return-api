1c417e673c56e921babad479a26d7796832879bc
suresh <sureshts2505@gmail,com>
2026-04-10 10:58:40 +0530

    Fix multer cloudinary upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,29 +3,17 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
     folder: "returnMe",
-    resource_type: "image",
-
-    
     public_id: (req, file) => {
-      return Date.now() + "-" + file.originalname;
+      return Date.now() + "-" + file.originalname.split(".")[0];
     },
   },
 });
 
 const upload = multer({
-  storage,
-
-  
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only image files allowed"), false);
-    }
-  },
+  storage
 });
 
 module.exports = upload;
